1ff1354a9d335965f9916a95658a2875fc920876
Robert Carter <kakulo83@gmail.com>
2019-08-03 18:28:05 -0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+};

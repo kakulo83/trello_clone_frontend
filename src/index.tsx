95508823935acbd90ea227d5dc27a4d88c0bfdfb
Robert Carter <kakulo83@gmail.com>
2019-09-02 22:28:41 -0700

    Add redux dev tool wrapper to store

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import { Provider } from 'react-redux'
 import AppContainer from "./containers/AppContainer";
 import appReducer from "./reducers/rootReducer";
 
-const store = createStore(appReducer)
+const store = createStore(
+  appReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
